33433d6ee3a938f4ae1f311ccdb46897ec1f20ba
Vincent Balbarin <vbalbarin@microsoft.com>
2025-09-29 12:06:33 -0400

    Adding code to accommodate VNET specified

--- application-workloads/active-directory/active-directory-new-domain/DSC/CreateADPDC.ps1
+++ application-workloads/active-directory/active-directory-new-domain/DSC/CreateADPDC.ps1
@@ -1,9 +1,79 @@
+function Convert-CIDRToSubnetMask {
+   [CmdletBinding()]
+   
+   param (
+       [Parameter(Mandatory = $true)]
+       [ValidateRange(0, 32)]
+       [int]$CIDR
+   )
+
+   $mask = ([math]::Pow(2, $CIDR) - 1) * [math]::Pow(2, (32 - $CIDR))
+   $bytes = [BitConverter]::GetBytes([UInt32]$mask)
+   (($bytes.Count - 1)..0 | ForEach-Object { [String]$bytes[$_] }) -join '.'
+}
+
+function Get-IPFilter {
+    [CmdletBinding()]
+
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$VirtualNetwork
+    )
+
+    $IPFilter = @('*', '*', '*', '*')
+    $Network = $VirtualNetwork.Split('/')[0]
+    $CIDR = $VirtualNetwork.Split('/')[1]
+    $NetworkMask = Convert-CIDRToSubnetMask -CIDR $CIDR
+
+    Write-Verbose $("Network: {0}" -f $Network)
+    Write-Verbose -Message $("CIDR: {0}" -f $CIDR)
+    Write-Verbose -Message $("Network Mask: {0}" -f $NetworkMask)
+
+    $NetworkMaskOctets = $NetworkMask.Split('.')
+    $NetworkOctets = $Network.Split('.')
+
+    $Idx = 0
+    foreach ($Octet in $NetworkOctets) {
+        $ValNetMaskOct = [int] $NetworkMaskOctets[$Idx]
+        Write-Verbose -Message ("NetworkMask Octet: {0}" -f $ValNetMaskOct)
+        
+        # NB, Technically, we should be returning a range of octet values if the netmask octet is in 1..254
+        # But this would be difficult to create a regex to specify this range
+        # Returning an * is good enough resolution.
+        
+        $IPFilter[$Idx] = if ($ValNetMaskOct -eq 255)
+        {
+            $NetworkOctets[$Idx]
+        } 
+        # elseif ($ValNetMaskOct -in 1..254)
+        # {
+        #     [string] (255 - $ValNetMaskOct)
+        # } 
+        # elseif ($ValNetMaskOct -eq 0) 
+        # {
+        #     '*'
+        # }
+        else 
+        {
+            '*'
+        }
+
+        $Idx++
+    }
+
+    return $IPFilter -join '.'
+
+}
+
 configuration CreateADPDC 
 { 
     param 
     ( 
         [Parameter(Mandatory)]
         [String]$DomainName,
+
+        [Parameter()]
+        [String]$VirtualNetwork = "",
 
         [Parameter(Mandatory)]
         [System.Management.Automation.PSCredential]$Admincreds,
@@ -14,7 +84,13 @@ configuration CreateADPDC
     
     Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
-    $Interface = Get-NetAdapter | Where Name -Like "Ethernet*" | Select-Object -First 1
+    
+    if ($VirtualNetwork.Length -eq 0) {
+        $Interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+    } else {
+        $Interface = Get-NetAdapter | Where-Object Name -Like $(Get-IPFilter -VirtualNetwork $VirtualNetwork) | Select-Object -First 1
+    }
+
     $InterfaceAlias = $($Interface.Name)
 
     Node localhost
